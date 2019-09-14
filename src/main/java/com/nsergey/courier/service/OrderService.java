f0910f047441cb61b7e0d20f3195d044320fee91
Nemchinov Sergey <googoodemon@gmail.com>
2019-09-14 18:25:39 +0700

    added OrderService class

---
+++ src/main/java/com/nsergey/courier/service/OrderService.java
@@ -0,0 +1,30 @@
+package com.nsergey.courier.service;
+
+import com.nsergey.courier.db.mapper.OrderMapper;
+import com.nsergey.courier.db.model.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class OrderService {
+
+    private final OrderMapper orderMapper;
+
+    @Autowired
+    public OrderService(OrderMapper orderMapper) {
+        this.orderMapper = orderMapper;
+    }
+
+    public List<Order> findAll() {
+        return orderMapper.findAll();
+    }
+
+    public List<Order> findAllByCourierId(Long courierId) {
+        return orderMapper.findAllByCourierId(courierId);
+    }
+
+}
