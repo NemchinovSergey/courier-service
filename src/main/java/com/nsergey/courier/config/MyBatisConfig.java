efbc61181ae82f28f2b38b82d33bcf7ddb283679
Nemchinov Sergey <googoodemon@gmail.com>
2019-09-15 21:47:08 +0700

    config classes

--- src/main/java/com/nsergey/courier/config/MyBatisConfig.java
+++ src/main/java/com/nsergey/courier/config/MyBatisConfig.java
@@ -1,9 +1,10 @@
 package com.nsergey.courier.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
-public class AppConfig {
-
+public class MyBatisConfig {
 
 }
