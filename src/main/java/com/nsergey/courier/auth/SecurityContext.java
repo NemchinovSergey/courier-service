4635202d48154c9eeeb6c217cf1dae2e764a5587
Nemchinov Sergey <googoodemon@gmail.com>
2019-09-15 23:35:04 +0700

    emulating of spring security context

--- src/main/java/com/nsergey/courier/auth/SecurityContext.java
+++ src/main/java/com/nsergey/courier/auth/SecurityContext.java
@@ -1,11 +1,19 @@
 package com.nsergey.courier.auth;
 
+/**
+ * Эмуляция контекста авторизации
+ */
 public abstract class SecurityContext {
 
     private static final long COURIER_ID = 1L;
+    private static final long OPERATOR_ID = 1001L;
 
     public static Long getAuthorisedCourierId() {
         return COURIER_ID;
+    }
+
+    public static Long getAuthorisedOperatorId() {
+        return OPERATOR_ID;
     }
 
 }
