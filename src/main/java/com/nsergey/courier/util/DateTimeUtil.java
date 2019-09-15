4e5ba8bb093372f5e8db928c655a29ef6929ee8a
Nemchinov Sergey <googoodemon@gmail.com>
2019-09-15 20:17:03 +0700

    method for instant formatting with timezone

--- src/main/java/com/nsergey/courier/util/DateTimeUtil.java
+++ src/main/java/com/nsergey/courier/util/DateTimeUtil.java
@@ -4,16 +4,24 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
+import static java.time.ZoneId.systemDefault;
+
 public abstract class DateTimeUtil {
 
     // todo вынести формат в настройки
     private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm";
 
-    // todo таймзона нужно брать в запросе web-клиента
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN).withZone(ZoneId.systemDefault());
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
 
+    /**
+     * Форматирует дату/время Instant в строку
+     */
     public static String toString(Instant instant) {
-        return instant == null ? "" : DATE_TIME_FORMATTER.format(instant);
+        return instant == null ? "" : DATE_TIME_FORMATTER.withZone(systemDefault()).format(instant);
+    }
+
+    public static String toString(Instant instant, ZoneId zoneId) {
+        return instant == null ? "" : DATE_TIME_FORMATTER.withZone(zoneId).format(instant);
     }
 
 }
